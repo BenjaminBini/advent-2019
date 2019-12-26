fdb2d33c640d8f6d695391d498141e2b804fca00
Benjamin Bini <benjamin.bini@sully-group.fr>
2019-12-26 14:41:20 +0100

    Day 11 (part 2)

--- src/io/bini/advent/Main.java
+++ src/io/bini/advent/Main.java
@@ -1,6 +1,16 @@
 package io.bini.advent;
 
+import io.bini.advent.day_1.Day1;
+import io.bini.advent.day_10.Day10;
 import io.bini.advent.day_11.Day11;
+import io.bini.advent.day_2.Day2;
+import io.bini.advent.day_3.Day3;
+import io.bini.advent.day_4.Day4;
+import io.bini.advent.day_5.Day5;
+import io.bini.advent.day_6.Day6;
+import io.bini.advent.day_7.Day7;
+import io.bini.advent.day_8.Day8;
+import io.bini.advent.day_9.Day9;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,7 +20,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         List<Day> daysToRun = new ArrayList<>();
-
+        daysToRun.add(new Day1());
+        daysToRun.add(new Day2());
+        daysToRun.add(new Day3());
+        daysToRun.add(new Day4());
+        daysToRun.add(new Day5());
+        daysToRun.add(new Day6());
+        daysToRun.add(new Day7());
+        daysToRun.add(new Day8());
+        daysToRun.add(new Day9());
+        daysToRun.add(new Day10());
         daysToRun.add(new Day11());
 
         for (Day day : daysToRun) {
